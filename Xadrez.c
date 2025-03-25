07eb9843ee87870cd57a0ee911e088a9a14b17b4
cauarz1 <rezendesilva01@outlook.com>
2025-03-25 20:41:52 +0000

    Movimentos Torre, Bispo e Rainha no xadrez.

---
+++ Xadrez.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int main(){
+
+//                          TORRE usando (FOR) 5 casas para direita
+    int t;
+    printf("Movimento da Torre: ");
+         for(t = 1; t <= 5; t++){
+            printf("Casa %d: Direita\n", t);    //imprimindo as direção do movimento
+     }
+printf("\n");
+
+
+
+
+
+
+
+
+
+
+
+//                 BISPO usando (WHILE) 5 casas na diagonal para cima e à direita
+printf("Movimento do Bispo: ");
+int b = 1;
+while (b <= 5){
+    printf("Casa %d: Cima, Direita\n", b);
+
+    b++;
+}
+printf("\n");
+
+
+
+
+
+
+
+
+
+
+
+//                          RAINHA usando (DO-WHILE) 8 casas para esquerda
+printf("Movimento da Ranha: ");
+int r = 1;
+do{
+    printf("Casa %d: Esquerda\n", r);
+    r++;
+
+}while(r <= 8);
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
